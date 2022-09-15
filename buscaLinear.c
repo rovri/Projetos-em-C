c48a278ced8bc3546aaf390eb55fc32f6c22639c
Daniel Roveri <111528313+rovri@users.noreply.github.com>
2022-09-15 14:03:36 -0300

    Update buscaLinear.c

--- buscaLinear.c
+++ buscaLinear.c
@@ -2,10 +2,15 @@
 
 int main(){
 
+    int buscaLinear(int num, int *vetor, int vTam);
     int v[] = {1,3,5,7,9};
     int vTam = 5;
+    int num;
 
+    printf("Qual numero voce deseja procurar?\n");
+    scanf("%i", &num);
 
+    printf("%i", buscaLinear(num, v, vTam));
 
 
 
@@ -13,11 +18,12 @@ int main(){
     return 0;
 }
 
-int buscaLinear(*vetor, vTam){
+int buscaLinear(int num, int *vetor, int vTam){
 
+    for(int i = 0; i < vTam; i++){
 
+        if(num == vetor[i]) return i;
+    }
 
-
-
-
+    return -1;
 }
