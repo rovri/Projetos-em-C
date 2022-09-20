f6d9282a2e01b8461a65b12e885397d5bdc442ee
Daniel Roveri <111528313+rovri@users.noreply.github.com>
2022-09-20 12:32:34 -0300

    Update buscaLinearVetorGrande.c

--- buscaLinearVetorGrande.c
+++ buscaLinearVetorGrande.c
@@ -3,11 +3,11 @@
 int main(){
 
     int buscaLinear(int num, int *vetor, int vTam);
-    int v[500000]; //Ao adicionar mais um zero, o programa nem roda.
-    int vTam = 500000;
+    int v[5000]; //Ao adicionar mais um zero, o programa nem roda.
+    int vTam = 5000;
     int num;
 
-    for(int i = 0; i < 500000; i++){
+    for(int i = 0; i < 5000; i++){
 
         v[i] = i * 2;
     }
@@ -15,7 +15,7 @@ int main(){
     printf("Qual numero voce deseja procurar?\n");
     scanf("%i", &num);
 
-    printf("%i", buscaLinear(3, v, vTam));
+    printf("%i", buscaLinear(num, v, vTam));
 
     return 0;
 }
@@ -24,7 +24,9 @@ int buscaLinear(int num, int *vetor, int vTam){
 
     for(int i = 0; i < vTam; i++){
 
-        if(num == vetor[i]) return i;
+        printf("iteração %i\n", i+1);
+
+        if(num == vetor[i+1]) return i;
     }
 
     return -1;
